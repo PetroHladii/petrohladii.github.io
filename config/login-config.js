af697f1a86b5d9519c0bb2ade37b5d4a4fb46b47
Tantsa98 <tantsa98@gmail.com>
2026-07-29 09:19:42 +0300

    20.07.0

--- config/login-config.js
+++ config/login-config.js
@@ -1,74 +1,37 @@
 export const CONFIG = {
 
-  /*
-   * Середовище
-   *
-   * "prod"
-   * "test"
-   */
+  /*Середовище "prod" "test"*/
+  ENV:"test",
 
-  ENV:
-    "test",
+  /*OTP*/
+  otpTtl:180,
+  otpSendCooldown:60,
+  otpMaxFailedAttempts:2,
 
-
-  /*
-   * OTP
-   */
-
-  otpTtl:
-    180,
-
-  otpSendCooldown:
-    60,
-
-  otpMaxFailedAttempts:
-    2,
-
-
-  /*
-   * Session
-   */
-
-  sessionTtl:
-    86400
+  /*Session*/
+  sessionTtl:86400
 
 };
 
-
-/*
- * OTP KV
- */
-
+/* OTP KV*/
 CONFIG.codesDb =
   CONFIG.ENV === "prod"
     ? "CODES"
     : "CODEST";
 
-
-/*
- * Users KV
- */
-
+/* Users KV*/
 CONFIG.usersDb =
   CONFIG.ENV === "prod"
     ? "USERS"
     : "USERST";
 
-
-/*
- * Sessions KV
- */
-
+/*Sessions KV*/
 CONFIG.sessionsDb =
   CONFIG.ENV === "prod"
     ? "SESSIONS"
     : "SESSIONST";
 
-
-/*
- * Email sender
- */
-
+/*Email sender*/
 CONFIG.mailSender =
   CONFIG.ENV === "prod"
     ? "noreply@mail.125.co.ua"
