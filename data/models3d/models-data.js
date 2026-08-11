fe5970c87a7078c1cb111ea65e312c98b1ab9d64
Tantsa98 <tantsa98@gmail.com>
2026-08-11 11:18:46 +0300

    11.08.2

--- data/models3d/models-data.js
+++ data/models3d/models-data.js
@@ -35,36 +35,104 @@ const MODELS = [
     id: 1,
     author: "2mb",
     category: "bomberh",
-    title: "Тест. Тримач кабелів для Вампіра",
+    title: "Тримач кабелів для Вампіра",
     description: "Тестовий запис для відображення можливостей функціоналу",
     preview: {title: "preview.webp", file: "preview.webp"},
     photos: [
-      {title: "Дрон Вампір",
-        file: "1.png"},
-      {title: "Монета",
-        file: "2.png"},
-      {title: "",
-        file: ""},
-      {title: "",
-        file: ""},
       {title: "",
         file: ""}
     ],
     models: [
       {title: "Тримач кабелю для Вампіра",
-        file: "Vamp_trimach_cable_VMP_000_041-R4.glb"},
-      {title: "",
-        file: ""},
-      {title: "",
-        file: ""}
+        file: "Vamp_trimach_cable_VMP_000_041-R4.glb"}
     ],
     files: [
       {title: "3D модель у форматі .stl",
-        file: "Vamp_trimach_cable.stl"},
-      {title: "",
-        file: ""},
+        file: "Vamp_trimach_cable.stl"}
+    ],
+    upd: "2026-01-01"
+  },
+  {
+    id: 2,
+    author: "2mb",
+    category: "bomberh",
+    title: "Шаблон під станок",
+    description: "Тестовий запис для відображення можливостей функціоналу",
+    preview: {title: "preview.webp", file: "preview.webp"},
+    photos: [
       {title: "",
         file: ""}
+    ],
+    models: [
+      {title: "Шаблон під станок",
+        file: "2_shablon_pid_stanok.glb"}
+    ],
+    files: [
+      {title: "3D модель у форматі .3mf",
+        file: "2_shablon_pid_stanok.3mf"}
+    ],
+    upd: "2026-01-01"
+  },
+  {
+    id: 3,
+    author: "2mb",
+    category: "bomberh",
+    title: "Кватро кришка",
+    description: "Тестовий запис для відображення можливостей функціоналу",
+    preview: {title: "preview.webp", file: "preview.webp"},
+    photos: [
+      {title: "",
+        file: ""}
+    ],
+    models: [
+      {title: "Кватро кришка",
+        file: "3_kvatro_kryshka.glb"}
+    ],
+    files: [
+      {title: "3D модель у форматі .3mf",
+        file: "3_kvatro_kryshka.3mf"}
+    ],
+    upd: "2026-01-01"
+  },
+  {
+    id: 4,
+    author: "2mb",
+    category: "bomberh",
+    title: "Кватро проставка",
+    description: "Тестовий запис для відображення можливостей функціоналу",
+    preview: {title: "preview.webp", file: "preview.webp"},
+    photos: [
+      {title: "",
+        file: ""}
+    ],
+    models: [
+      {title: "Кватро проставка",
+        file: "4_kvatro_prostavka.glb"}
+    ],
+    files: [
+      {title: "3D модель у форматі .3mf",
+        file: "4_kvatro_prostavka.3mf"}
+    ],
+    upd: "2026-01-01"
+  },
+  {
+    id: 5,
+    author: "2mb",
+    category: "bomberh",
+    title: "Проставка бустер 8 Ват",
+    description: "Тестовий запис для відображення можливостей функціоналу",
+    preview: {title: "preview.webp", file: "preview.webp"},
+    photos: [
+      {title: "",
+        file: ""}
+    ],
+    models: [
+      {title: "Проставка бустер 8 Ват",
+        file: "5_prostavka_buster_8_vat.glb"}
+    ],
+    files: [
+      {title: "3D модель у форматі .3mf",
+        file: "5_prostavka_buster_8_vat.3mf"}
     ],
     upd: "2026-01-01"
   }
