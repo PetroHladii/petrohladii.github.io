2bf9e4ec6ecac379bf2db2c0ab02ae8b93a0f4c3
Tantsa98 <tantsa98@gmail.com>
2026-08-13 14:44:49 +0300

    13/08/2026

--- data/knowledge/knowledge-data.js
+++ data/knowledge/knowledge-data.js
@@ -37,15 +37,10 @@ const KNOWLEDGE = [
 
   {
     id: 1,
-
     author: "bbps",
-
     category: "fpv",
-
     title: "FPV: Основи",
-
     contentFile: "1.html",
-
     files: [
       {
         title: "Антени для FPV дронів",
@@ -72,59 +67,27 @@ const KNOWLEDGE = [
         file: "radio_sticks.pdf"
       }
     ],
-
     photos: [
       {
         title: "",
         file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
+      }
     ],
-
     videos: [
       {
         title: "",
         file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
-      {
-        title: "",
-        file: ""
-      },
+      }
     ],
     updated: "2026-01-01"
-
   },
 
-    {
+  {
     id: 2,
-
     author: "2mb",
-
     category: "bomberh",
-
     title: "Документація VAMPIRE",
-
     contentFile: "2.html",
-
     files: [
       {
         title: "Vampire КЛЕ",
@@ -135,18 +98,12 @@ const KNOWLEDGE = [
         file: "vampire_gen3.pdf"
       }
     ],
-
     photos: [
-      {
-        title: "",
-        file: ""
-      },
       {
         title: "",
         file: ""
       }
     ],
-
     videos: [
       {
         title: "",
@@ -154,20 +111,14 @@ const KNOWLEDGE = [
       }        
     ],
     updated: "2026-01-01"
-
   },
 
-    {
+  {
     id: 3,
-
     author: "bbps",
-
     category: "bomberh",
-
     title: "Важкі бомбери: Документація",
-
     contentFile: "3.html",
-
     files: [
       {
         title: "Важкі бомбери - шпаргалка",
@@ -191,18 +142,12 @@ const KNOWLEDGE = [
         file: "antennas_ttc.pdf"
       }
     ],
-
     photos: [
-      {
-        title: "",
-        file: ""
-      },
       {
         title: "",
         file: ""
       }
     ],
-
     videos: [
       {
         title: "Підготовка БК перед вильотом",
@@ -210,60 +155,41 @@ const KNOWLEDGE = [
       }        
     ],
     updated: "2026-01-01"
-
   },
 
-    {
+  {
     id: 4,
-
     author: "bbps",
-
     category: "bomberh",
-
     title: "Переобладнання Вампіра на R2D2",
-
     contentFile: "4.html",
-
     files: [
       {
         title: "",
         file: ""
       }
     ],
-
     photos: [
       {
         title: "",
         file: ""
-      },
-      {
-        title: "",
-        file: ""
       }
     ],
-
     videos: [
       {
         title: "",
         file: ""
-      }
-              
+      }        
     ],
     updated: "2026-01-01"
-
   },
 
-    {
+  {
     id: 5,
-
     author: "bbps",
-
     category: "mavic",
-
     title: "Розвідувальні коптери: Основи",
-
     contentFile: "5.html",
-
     files: [
       {
         title: "Розвідувальні коптери: Основи",
@@ -286,11 +212,9 @@ const KNOWLEDGE = [
         file: "kropyva_vezha_discord_milchat.pdf"
       }
     ],
-
     photos: [
       
     ],
-
     videos: [
       {
         title: "Скиди на розвідувальних коптерах",
@@ -298,7 +222,158 @@ const KNOWLEDGE = [
       }      
     ],
     updated: "2026-01-01"
+  },
 
+  {
+    id: 6,
+    author: "brigade",
+    category: "a2",
+    title: "A2 BOT: Подача доповіді 'Виконане завдання БпЛА'",
+    contentFile: "6.html",
+    files: [
+      {
+        title: "Вхід в систему A2 BOT",
+        file: "a2_login.pdf"
+      },
+      {
+        title: "А2 Bot: FPV: 'Виконане завдання (БпЛА)'",
+        file: "a2_fpv_mission.pdf"
+      },
+      {
+        title: "А2 Bot: Важкий бомбер: 'Виконане завдання (БпЛА)'",
+        file: "a2_heavy_bomber_mission.pdf"
+      },
+      {
+        title: "А2 Bot: Ударне крило: 'Виконане завдання (БпЛА)'",
+        file: "a2_strike_wing_mission.pdf"
+      },
+      {
+        title: "А2 Bot: Розвідувальний коптер: 'Виконане завдання (БпЛА)'",
+        file: "a2_recon_copter_mission.pdf"
+      }
+    ],
+    photos: [
+      ],
+    videos: [
+      {
+        title: "Скиди на розвідувальних коптерах",
+        file: "bc684d8088b4248edc3b9be77b094be6"
+      }      
+    ],
+    updated: "2026-01-01"
+  },
+
+  {
+    id: 7,
+    author: "brigade",
+    category: "a2",
+    title: "A2 BOT: Подача доповіді 'Втрата БпЛА'",
+    contentFile: "7.html",
+    files: [
+      {
+        title: "Вхід в систему A2 BOT",
+        file: "a2_login.pdf"
+      },
+      {
+        title: "А2 Bot: Важкий бомбер: 'Втрата (БпЛА)'",
+        file: "a2_heavy_bomber_loss.pdf"
+      },
+      {
+        title: "А2 Bot: Розвідувальне крило: 'Втрата (БпЛА)'",
+        file: "a2_recon_wing_loss.pdf"
+      },
+      {
+        title: "А2 Bot: Розвідувальний коптер: 'Втрата (БпЛА)'",
+        file: "a2_recon_copter_loss.pdf"
+      }
+    ],
+    photos: [ 
+    ],
+    videos: [
+      {
+        title: "Скиди на розвідувальних коптерах",
+        file: "bc684d8088b4248edc3b9be77b094be6"
+      }      
+    ],
+    updated: "2026-01-01"
+  },
+
+  {
+    id: 8,
+    author: "brigade",
+    category: "fa",
+    title: "FA: Верифікація",
+    contentFile: "8.html",
+    files: [
+      {
+        title: "Вхід в систему FA",
+        file: "fa_login.pdf"
+      },
+      {
+        title: "Настанова Autel Evo Max 4",
+        file: "autel_evo_max_manual_v1.pdf"
+      },
+      {
+        title: "Інструкція для операторів Мавіка",
+        file: "mavic_operators_instruction.pdf"
+      },
+      {
+        title: "Matrice 4T/4E",
+        file: "matrice4te.pdf"
+      },
+      {
+        title: "Додаткове ПЗ: Кропива, Вежа, Діскорд, Мілчат",
+        file: "kropyva_vezha_discord_milchat.pdf"
+      }
+    ],
+    photos: [
+    ],
+    videos: [
+      {
+        title: "Скиди на розвідувальних коптерах",
+        file: "bc684d8088b4248edc3b9be77b094be6"
+      }      
+    ],
+    updated: "2026-01-01"
+  },
+
+  {
+    id: 9,
+    author: "brigade",
+    category: "fa",
+    title: "A2 Bot/FA: Резерв",
+    contentFile: "9.html",
+    files: [
+      {
+        title: "Вхід в систему A2 BOT",
+        file: "a2_login.pdf"
+      },
+      {
+        title: "Вхід в систему FA",
+        file: "autel_evo_max_manual_v1.pdf"
+      },
+      {
+        title: "Інструкція для операторів Мавіка",
+        file: "mavic_operators_instruction.pdf"
+      },
+      {
+        title: "Matrice 4T/4E",
+        file: "matrice4te.pdf"
+      },
+      {
+        title: "Додаткове ПЗ: Кропива, Вежа, Діскорд, Мілчат",
+        file: "kropyva_vezha_discord_milchat.pdf"
+      }
+    ],
+    photos: [ 
+    ],
+    videos: [
+      {
+        title: "Скиди на розвідувальних коптерах",
+        file: "bc684d8088b4248edc3b9be77b094be6"
+      }      
+    ],
+    updated: "2026-01-01"
   }
 
 ];
